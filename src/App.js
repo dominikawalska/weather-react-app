289186bcd4d8749caebaa6c085801aee365965bb
dominikawalska <dominikawalska@gmail.com>
2022-05-01 21:23:51 +0200

    Added "MainWindow"component and links to GitHub

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Search from "./Search";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
+import MainWindow from "./MainWindow";
 
 export default function App() {
   return (
@@ -9,6 +10,13 @@ export default function App() {
       <div className="weather-app">
         <div className="weather-frame">
           <Search />
+          <MainWindow />
+          <div>
+            <a href="https://github.com/dominikawalska/weather-react-app">
+              Open-sourced code
+            </a>{" "}
+            by <a href="https://github.com/dominikawalska">Dominika Walska</a>
+          </div>
         </div>
       </div>
     </Container>
